6ab8cd870f102114234d3b860cb2a59ae18a324f
qiyubrother <13240137763@163.com>
2020-10-21 15:18:21 +0800

    添加项目文件。

---
+++ RtmpVLCTV/FrmTV.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace RtmpVLCTV
+{
+    partial class FrmTV
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmTV));
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.menuStripChannel = new System.Windows.Forms.MenuStrip();
+            this.menuSelectChannel = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.menuStripChannel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView1.Location = new System.Drawing.Point(0, 25);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(800, 425);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // menuStripChannel
+            // 
+            this.menuStripChannel.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuSelectChannel});
+            this.menuStripChannel.Location = new System.Drawing.Point(0, 0);
+            this.menuStripChannel.Name = "menuStripChannel";
+            this.menuStripChannel.Size = new System.Drawing.Size(800, 25);
+            this.menuStripChannel.TabIndex = 1;
+            this.menuStripChannel.Text = "menuStrip1";
+            // 
+            // menuSelectChannel
+            // 
+            this.menuSelectChannel.Name = "menuSelectChannel";
+            this.menuSelectChannel.Size = new System.Drawing.Size(44, 21);
+            this.menuSelectChannel.Text = "选台";
+            this.menuSelectChannel.Click += new System.EventHandler(this.menuSelectChannel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoView1);
+            this.Controls.Add(this.menuStripChannel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStripChannel;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "湖南卫视";
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.menuStripChannel.ResumeLayout(false);
+            this.menuStripChannel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private System.Windows.Forms.MenuStrip menuStripChannel;
+        private System.Windows.Forms.ToolStripMenuItem menuSelectChannel;
+    }
+}
+
---
+++ RtmpVLCTV/FrmTV.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RtmpVLCTV
+{
+    public partial class FrmTV : Form
+    {
+        public FrmTV()
+        {
+            InitializeComponent();
+        }
+
+        // install-package LibVLCSharp.WinForms
+        // install-package VideoLAN.LibVLC.Windows
+        Dictionary<string, string> dictChannel = new Dictionary<string, string>();
+        Form frmSelChannel = new Form { Width = 750, Height = 300 };
+        LibVLCSharp.Shared.LibVLC libvlc = null;
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            var flow = new FlowLayoutPanel();
+            frmSelChannel.Controls.Add(flow);
+            frmSelChannel.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            frmSelChannel.StartPosition = FormStartPosition.CenterScreen;
+            frmSelChannel.Text = "选台控制面板";
+            flow.Dock = DockStyle.Fill;
+            frmSelChannel.KeyPreview = true;
+            frmSelChannel.KeyDown += FrmSelChannel_KeyDown;
+            var fileName = Path.Combine(Application.StartupPath, "sys.json");
+            var s = File.ReadAllText(fileName);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(s);
+            var data = jo["data"] as JArray;
+            foreach(JObject jitem in data)
+            {
+                var name = jitem["Name"].ToString();
+                var url = jitem["URL"].ToString();
+                if (!dictChannel.ContainsKey(name))
+                {
+                    dictChannel.Add(name, url);
+                    var btn = new Button { Text = name, Tag = url };
+                    btn.Click += SelectChannelButton_Click;
+                    flow.Controls.Add(btn);
+                }
+            }
+            LibVLCSharp.Shared.Core.Initialize();
+            libvlc = new LibVLCSharp.Shared.LibVLC();
+            videoView1.MediaPlayer = new LibVLCSharp.Shared.MediaPlayer(libvlc);
+
+            KeyPreview = true;
+            KeyDown += FrmTV_KeyDown;
+        }
+
+        private void FrmTV_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                frmSelChannel.Hide();
+            }
+            else if (e.KeyCode == Keys.F1)
+            {
+                menuSelectChannel.PerformClick();
+            }
+        }
+
+        private void FrmSelChannel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                frmSelChannel.Hide();
+            }
+        }
+
+        public void NavToChannel(string channelName, string url)
+        {
+            Text = channelName;
+            var media = new LibVLCSharp.Shared.Media(libvlc, new Uri(url));
+            videoView1.MediaPlayer.Play(media);
+        }
+
+        private void SelectChannelButton_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                var url = btn.Tag as string;
+                if (url != string.Empty)
+                {
+                    NavToChannel(btn.Text, url);
+                }
+            }
+        }
+
+        private void menuSelectChannel_Click(object sender, EventArgs e)
+        {
+            frmSelChannel.Show();
+        }
+    }
+}
